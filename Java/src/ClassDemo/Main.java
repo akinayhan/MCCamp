362c40732c84486020e256f9fc8fd50bf58fb41d
akın ayhan <akinayhan94@gmail.com>
2022-07-27 19:12:28 +0300

    27.07.2022

---
+++ Java/src/ClassDemo/Main.java
@@ -0,0 +1,12 @@
+package ClassDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		
+		int sonuc = calculator.Plus(3,4);
+		System.out.println(sonuc);
+	}
+
+}
