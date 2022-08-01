7b9d7d43ba38a918052a5239f85733ae1e1d3c87
akın ayhan <akinayhan94@gmail.com>
2022-08-01 18:59:45 +0300

    01.08.2022

---
+++ Java/src/ClassesWithAttributes/Main.java
@@ -0,0 +1,22 @@
+package ClassesWithAttributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product = new Product();
+		product.setName("Laptop");
+		product.setId(1);
+		product.setDescription("Asus Laptop");
+		product.setPrice(25000);
+		product.setStockAmount(3);
+		
+		
+		
+		
+		ProductManager productManager = new ProductManager();
+		productManager.add(product);
+		
+		System.out.println(product.getCode());
+	}
+
+}
---
+++ Java/src/ClassesWithAttributes/ProductManager.java
@@ -0,0 +1,9 @@
+package ClassesWithAttributes;
+
+public class ProductManager {
+	public void add(Product product) {
+		//JDBC
+		System.out.println("ürün eklendi : " + product.getName());
+	}
+	
+}
